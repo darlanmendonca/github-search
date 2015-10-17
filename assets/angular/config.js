44519892c6d98ff5a1258350e1ff16911bf14c5a
darlanmendonca <darlanmendonca@gmail.com>
2015-10-17 16:35:20 -0300

    finish style

--- assets/angular/config.js
+++ assets/angular/config.js
@@ -4,7 +4,11 @@ angular
   .module('github-search')
   .config(config);
 
-function config (cfpLoadingBarProvider) {
+function config (cfpLoadingBarProvider, $mdThemingProvider) {
 	// disable spinner from angular-loading-bar
 	cfpLoadingBarProvider.includeSpinner = false;
+
+	$mdThemingProvider.theme('default')
+    .primaryPalette('blue')
+    .accentPalette('blue-grey');
 }
